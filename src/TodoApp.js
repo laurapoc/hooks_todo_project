2b2881d6b5c08c18685366dc54a6c7119d9a6355
Laura P <laura.pociuniene@gmail.com>
2021-05-05 15:35:18 +0300

    first commit

---
+++ src/TodoApp.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import TodoForm from "./TodoForm";
+
+export default function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: "Clean Fich Tank", completed: false },
+    { id: 2, task: "Wash car", completed: true },
+    { id: 3, task: "Grow hear", completed: false },
+  ];
+  const [todos, setTodos] = useState(initialTodos);
+
+  const addTodo = (newTodoText) => {
+    setTodos([...todos, { id: 4, task: newTodoText, completed: false }]);
+  };
+  return (
+    <Paper
+      style={{
+        padding: 0,
+        margin: 0,
+        height: "100vh",
+        backgroundColor: "#fafafa",
+      }}
+      elevation={0}
+    >
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
+        <Toolbar>
+          <Typography color="inherit">TODOS WITH HOOKS</Typography>
+        </Toolbar>
+      </AppBar>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} />
+    </Paper>
+  );
+}
+
+// - TodoApp
+// - TodoForm
+// - Todolist
+// - TodoItem
+
+// id, task, completed
---
+++ src/TodoList.js
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+
+export default function TodoList(props) {
+  return (
+    <Paper>
+      <List>
+        {props.todos.map((todo) => (
+          <Fragment>
+            <ListItem>
+              <ListItemText>{todo.task}</ListItemText>
+            </ListItem>
+            <Divider />
+          </Fragment>
+        ))}
+      </List>
+    </Paper>
+  );
+}
